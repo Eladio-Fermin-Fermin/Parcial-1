685b4e3ff045dd73130b9212a7f3e2fbbb72f25c
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-06-15 18:33:14 -0400

    Se añadio la entidad Articulos en la carpeta Entidades

---
+++ Entidades/Articulos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Parcial1.Entidades
+{
+    public class Articulos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public int Costo { get; set; }
+        public int ValorInventario { get; set; }
+    }
+}
