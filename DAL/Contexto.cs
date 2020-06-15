debf6806db931fe31c8ee2124cb8f9d70bf33b4a
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-06-15 17:47:22 -0400

    Se creo la herencia en la clase Context con DbContext en la carpeta DAL

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial1.DAL
+{
+    class Contexto : DbContext 
+    {
+
+    }
+}
